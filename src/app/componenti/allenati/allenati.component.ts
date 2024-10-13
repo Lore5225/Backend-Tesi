f20aefec1d0581b17bd7a1df0ede5c310d3826c8
Lore25 <varsallonalorenzo@gmail.com>
2024-10-13 17:50:08 +0200

    fixed css and name of file

--- src/app/componenti/allenati/allenati.component.ts
+++ src/app/componenti/allenati/allenati.component.ts
@@ -12,28 +12,35 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./allenati.component.css'],
 })
 export class AllenatiComponent {
-  pdfUrl: string | null = null; // URL for the PDF blob
-  loading: boolean = false; // Indica se è in corso la generazione dell'esame
+  pdfUrl: string | null = null; 
+  loading: boolean = false; 
   selectedFile: any;
+  filename: string = '';
 
   constructor(private generatoriEsamiService: GeneratoreEsamiService) {}
 
   generateExam(examType: 'sql' | 'erm') {
-    this.loading = true; // Inizia il caricamento
+    this.loading = true; 
 
     this.generatoriEsamiService.generateExam(examType).subscribe({
-      next: (blob: Blob) => {
-        // Crea un URL per il blob e assegnalo a pdfUrl
+      next: (response: Blob) => { 
+        const blob: Blob = new Blob([response], { type: 'application/pdf' });
         this.pdfUrl = URL.createObjectURL(blob);
-        this.loading = false; // Ferma il caricamento
+        this.filename = `Esame_${examType}_${new Date().toISOString().slice(0, 10)}.pdf`; 
+        
+       
+        const audio = new Audio('assets/suono.mp3'); 
+        audio.play().catch((error) => console.error('Error playing audio', error));
+
+        this.loading = false; 
       },
       error: (error) => {
         console.error('Error generating exam', error);
-        this.loading = false; // Ferma il caricamento anche in caso di errore
+        this.loading = false; 
       },
     });
   }
-
+  
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
   }
@@ -46,13 +53,18 @@ export class AllenatiComponent {
 
     this.loading = true;
 
-    // Invia il file selezionato al backend per generare la soluzione SQL
+    
     this.generatoriEsamiService
       .generateSolutionSQL(this.selectedFile)
       .subscribe({
-        next: (blob: Blob) => {
-          // Crea un URL per il blob della soluzione SQL e assegnalo a pdfUrl
+        next: (response: Blob) => {
+          const blob: Blob = new Blob([response], { type: 'application/pdf' });
           this.pdfUrl = URL.createObjectURL(blob);
+          this.filename = `SoluzioneGenerata_${new Date().toISOString().slice(0, 10)}.pdf`; 
+
+          // Inizializza il suono ogni volta che viene chiamato
+          const audio = new Audio('assets/suono.mp3'); 
+          audio.play().catch((error) => console.error('Error playing audio', error));
           this.loading = false;
         },
         error: (error) => {
@@ -62,15 +74,15 @@ export class AllenatiComponent {
       });
   }
 
-  downloadPdfFile(filename: string) {
+  downloadPdfFile() {
     if (this.pdfUrl) {
-      const link = document.createElement('a');
+      const link = document.createElement('a');      
       link.href = this.pdfUrl;
-      link.download = filename;
-
+      link.download = this.filename;
+  
       link.click();
       URL.revokeObjectURL(this.pdfUrl);
-      this.pdfUrl = null; // Resetta l'URL
+      this.pdfUrl = null; 
     } else {
       console.warn('Nessun file PDF generato da scaricare.');
     }
