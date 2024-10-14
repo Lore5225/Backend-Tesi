31e4989b89e9501892ade9b7bfb663f0b345ebac
Lore25 <varsallonalorenzo@gmail.com>
2024-10-14 19:05:04 +0200

    added message error to login and registration

--- src/app/componenti/login/login.component.ts
+++ src/app/componenti/login/login.component.ts
@@ -5,7 +5,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthServiceService } from '../../Services/auth-service.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -14,17 +19,19 @@ import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, 
-            MatFormFieldModule, 
-            MatInputModule, 
-            MatButtonModule, 
-            MatSelectModule, 
-            ReactiveFormsModule, 
-            RouterLink, 
-            MatCheckboxModule, 
-            HttpClientModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    RouterLink,
+    MatCheckboxModule,
+    HttpClientModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   accForm!: FormGroup;
@@ -44,8 +51,11 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.accForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-      is_professor: new FormControl(this.isChecked)
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      is_professor: new FormControl(this.isChecked),
     });
 
     this.authService.checkSession().subscribe({
@@ -56,7 +66,7 @@ export class LoginComponent implements OnInit {
       },
       error: (err) => {
         console.error('Errore verifica sessione:', err);
-      }
+      },
     });
   }
 
@@ -65,13 +75,16 @@ export class LoginComponent implements OnInit {
       const formData = this.accForm.value;
       this.authService.login(formData).subscribe({
         next: (response) => {
-          this.snackBar.open('Login effettuato con successo!', 'Chiudi', { duration: 3000 });
+          this.snackBar.open('Login effettuato con successo!', 'Chiudi', {
+            duration: 3000,
+          });
           this.router.navigate(['/home']);
         },
         error: (err) => {
-          const errorMessage = err.error.message || 'Errore durante il login. Riprova.';
+          const errorMessage =
+            err.error.message || 'Errore durante il login. Riprova.';
           this.snackBar.open(errorMessage, 'Chiudi', { duration: 3000 });
-        }
+        },
       });
     }
   }
--- src/app/componenti/registrazione/registrazione.component.ts
+++ src/app/componenti/registrazione/registrazione.component.ts
@@ -4,24 +4,31 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { AuthServiceService } from '../../Services/auth-service.service'; 
+import { AuthServiceService } from '../../Services/auth-service.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-registrazione',
   standalone: true,
-  imports: [CommonModule, 
-            MatFormFieldModule, 
-            MatInputModule, 
-            MatButtonModule,
-            MatSelectModule, 
-            ReactiveFormsModule, 
-            RouterLink],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    RouterLink,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './registrazione.component.html',
-  styleUrls: ['./registrazione.component.css']
+  styleUrls: ['./registrazione.component.css'],
 })
 export class RegistrazioneComponent implements OnInit {
   regForm!: FormGroup;
@@ -34,22 +41,36 @@ export class RegistrazioneComponent implements OnInit {
 
   ngOnInit(): void {
     this.regForm = new FormGroup({
-      nome: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z ]*$')]), 
-      cognome: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z ]*$')]), 
-      matricola: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10}$')]), 
+      nome: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('^[a-zA-Z ]*$'),
+      ]),
+      cognome: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('^[a-zA-Z ]*$'),
+      ]),
+      matricola: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]{10}$'),
+      ]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
     });
 
     this.authService.checkSession().subscribe({
       next: (response) => {
         if (response.logged_in) {
-          this.router.navigate(['/home']); 
+          this.router.navigate(['/home']);
         }
       },
       error: (err) => {
         console.error('Errore verifica sessione:', err);
-      }
+      },
     });
   }
 
@@ -58,13 +79,17 @@ export class RegistrazioneComponent implements OnInit {
       const formData = this.regForm.value;
       this.authService.signup(formData).subscribe({
         next: (response) => {
-          this.snackBar.open('Registrazione effettuata con successo!', 'Chiudi', { duration: 3000 });
-          this.router.navigate(['/home']); 
+          this.snackBar.open(
+            'Registrazione effettuata con successo!',
+            'Chiudi',
+            { duration: 3000 }
+          );
+          this.router.navigate(['/home']);
         },
         error: (err) => {
-          this.snackBar.open('Errore durante la registrazione. Riprova.', 'Chiudi', { duration: 3000 });
-          console.error('Errore registrazione:', err);
-        }
+          const errorMessages = err.error.message;
+          this.snackBar.open(errorMessages, 'Chiudi', { duration: 3000 });
+        },
       });
     }
   }
